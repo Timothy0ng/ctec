30724e6204a2ef0692bf70d636e8f443fd17bc7b
Timothy0ng <2101237b@student.tp.edu.sg>
2022-08-20 15:23:26 +0800

    a

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -153,7 +153,7 @@ function editReview(element) {
     currentIndex = item;
 
     // document.getElementById("editnickname").value = review_array[item].reviews_UserId;
-    document.getElementById("editnickname").value = review_array[item].username;
+    document.getElementById("editnickname").value = current_user.username;
     document.getElementById("edituserReviews").value = review_array[item].review;
     console.log(review_array[item].rating);
     // displayColorPopcorn('editpop', review_array[item].rating);
